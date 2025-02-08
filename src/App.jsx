6abc1275c756e041057deb8ab7a9ca4c80714ab0
user-azizul <azizul.contact.me@gmail.com>
2025-02-08 21:37:21 +0300

    add Footer component with styling and integrate into App

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,21 @@ import { Route, Routes } from "react-router";
 import Home from "./Pages/Home/Home";
 import Cart from "./Pages/Cart/Cart";
 import PlaceOrder from "./Pages/PlaceOrder/PlaceOrder";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/order" element={<PlaceOrder />} />
-      </Routes>
-    </div>
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Footer.css";
+import { assets } from "../../assets/frontend_assets/assets";
+
+function Footer() {
+  return (
+    <div className="footer" id="footer">
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Repellendus earum excepturi a illo fugiat velit tenetur voluptates
+            aspernatur accusantium totam!
+          </p>
+
+          <div className="footer-social">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2025 &copy; Tomato.com - All Right Reserved
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
