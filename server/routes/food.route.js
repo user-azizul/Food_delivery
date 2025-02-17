63e495a1681d79700c490bff815f1eaa180d162a
user-azizul <azizul.contact.me@gmail.com>
2025-02-18 01:44:58 +0300

    add food list endpoint

--- server/routes/food.route.js
+++ server/routes/food.route.js
@@ -1,21 +1,21 @@
-import express from 'express';
+import express from "express";
 
-import {addFood} from '../controllers/food.controller.js';
+import { addFood, listFood } from "../controllers/food.controller.js";
 
-import multer from 'multer';
+import multer from "multer";
 
- const foodRouter = express.Router();
-
+const foodRouter = express.Router();
 
 // image upload
-const  storage = multer.diskStorage({
-    destination:'uploads',
-    filename:(req,file,cb)=>{
-        cb(null,`${Date.now()}_${file.originalname}`)
-    }
-})
-const upload = multer({storage:storage})  
+const storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 
-foodRouter.post("/add",upload.single('image'),addFood)
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
 
-export default foodRouter
+export default foodRouter;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,29 @@
-import express from 'express'
-import cors from  'cors'
-import { connectDB } from './config/db.js';
-import foodRouter from './routes/food.route.js';
-
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import foodRouter from "./routes/food.route.js";
 
 // app config
 
 const app = express();
 const port = 4000;
 
-// middleware 
+// middleware
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-
-app.get('/',(req,res)=>{
-res.send('Api working')
-})
-// Database connection 
+app.get("/", (req, res) => {
+  res.send("Api working");
+});
+// Database connection
 connectDB();
 
 // api endpoints
-app.use("api/food",foodRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 // listen
 
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
