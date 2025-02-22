75d4e67288ac7a4dc474956410cacaa73622fd10
user-azizul <azizul.contact.me@gmail.com>
2025-02-22 23:58:53 +0300

    fetch list item and display to frontend

--- admin/src/Pages/List/List.jsx
+++ admin/src/Pages/List/List.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
-import './List.css'
+import React from "react";
+import "./List.css";
+import axios from "axios";
+import { toast } from "react-toastify";
 function List() {
+  const [list, setList] = React.useState([]);
+  const URL = "http://localhost:4000";
+  async function fetchList() {
+    try {
+      const { data } = await axios.get(`${URL}/api/food/list`);
+      if (data.success) {
+        setList(data.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong on getting food list", error);
+      toast.error("Something went wrong on getting food list");
+    }
+  }
+
+  React.useEffect(() => {
+    fetchList();
+  }, []);
+
   return (
-    <div>
-      
+    <div className="list add flex-col">
+      <p className="list-title">All Food List</p>
+      <div className="list-table">
+        <div className="list-table-format title">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Category</b>
+          <b>Price</b>
+          <b>Action</b>
+        </div>
+        {list.map((item) => (
+          <div className="list-table-format" key={item.id}>
+            <img src={`${URL}/images/${item.image}`} alt={item.name} />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>{item.price}</p>
+            <p>X</p>
+          </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default List
+export default List;
--- server/controllers/food.controller.js
+++ server/controllers/food.controller.js
@@ -28,47 +28,58 @@ const addFood = async (req, res) => {
 const listFood = async (req, res) => {
   try {
     const foods = await FoodModel.find({});
+
+    if (!foods.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No food items found",
+      });
+    }
+
     return res.json({ success: true, data: foods });
   } catch (error) {
-    console.error("Error listing food:", error);
+    console.error("Error listing food:", error.message);
     return res.status(500).json({
       success: false,
-      message: "Something went wrong while listing food",
+      message: "Internal server error while listing food",
     });
   }
 };
 
 // remove food
 
-
 const removeFood = async (req, res) => {
-    try {
-        const { _id } = req.body;
+  try {
+    const { _id } = req.body;
 
-        if (!_id) {
-            return res.json({ success: false, message: "Food ID is required" });
-        }
-
-        const food = await FoodModel.findById(_id);
-        if (!food) {
-            return res.json({ success: false, message: "Food not found" });
-        }
-
-        // Delete food image from uploads if it exists
-        if (food.image) {
-            const imagePath = `uploads/${food.image}`;
-            fs.unlink(imagePath, (err) => {
-                if (err) console.error("Error deleting image:", err);
-            });
-        }
-
-        await FoodModel.findByIdAndDelete(_id);
-        return res.json({ success: true, message: "Food deleted successfully" });
-
-    } catch (error) {
-        console.error("Something went wrong while deleting food:", error);
-        return res.status(500).json({ success: false, message: "Something went wrong while deleting food" });
+    if (!_id) {
+      return res.json({ success: false, message: "Food ID is required" });
     }
+
+    const food = await FoodModel.findById(_id);
+    if (!food) {
+      return res.json({ success: false, message: "Food not found" });
+    }
+
+    // Delete food image from uploads if it exists
+    if (food.image) {
+      const imagePath = `uploads/${food.image}`;
+      fs.unlink(imagePath, (err) => {
+        if (err) console.error("Error deleting image:", err);
+      });
+    }
+
+    await FoodModel.findByIdAndDelete(_id);
+    return res.json({ success: true, message: "Food deleted successfully" });
+  } catch (error) {
+    console.error("Something went wrong while deleting food:", error);
+    return res
+      .status(500)
+      .json({
+        success: false,
+        message: "Something went wrong while deleting food",
+      });
+  }
 };
 
-export { addFood, listFood,removeFood };
+export { addFood, listFood, removeFood };
